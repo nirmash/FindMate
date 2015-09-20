8873ee483f4dedaa5bf0e633d0559fed3c5a4946
System Administrator <root@Nirs-MacBook-Air.local>
2015-09-19 20:12:08 -0700

    commit 2

---
+++ tables/TodoItem.js
@@ -0,0 +1,25 @@
+// TodoItem.js
+var todoTable = require('azure-mobile-apps').table('TodoItem');
+todoTable.auth = true;
+// Export our table
+module.exports = todoTable;
+//todoTable.columns = {"text":"string", "complete":"boolean"} //Dynamic schema will create these for us. It's on by default.
+todoTable.dynamicSchema = true; // this is the default setting
+
+todoTable.read(function(context){
+  return context.execute();
+});
+// TodoItem.js continued
+// Attach a user id to the item that was inserted
+table.insert(function (context) {
+  context.item.user = context.user.id;
+  return context.execute();
+});
+
+// Only return items where the user id matches the current user
+table.read(function (context) {
+  context.query.where({
+    user: context.user.id
+  });
+  return context.execute();
+});
